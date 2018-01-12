06c554f1ca2ceb86ca8a3727ee9cfe2398b1fdda
Felipe Silva <felipe.dacs@gmail.com>
2018-01-12 16:10:32 -0200

    att com show, hide, img rd e legenda

--- assets/js/draganddrop.js
+++ assets/js/draganddrop.js
@@ -5,18 +5,35 @@ var draggableConfig = {
 };
 
 var sortableConfig = {
-  placeholder: "AAAA",
+  activate: function( event, ui ) {
+    $( ".player" ).addClass("ui-player-state-ativado");
+    $(".legenda-player").show();
+  },
+  deactivate: function( event, ui ) {
+    $( ".player" ).removeClass("ui-player-state-ativado");
+    $(".legenda-player").hide();
+  },
   update: function( event, ui ) {
     $( ".success" ).fadeIn( 300 ).delay( 1500 ).fadeOut( 400 );
   }
 };
 
 var droppableConfig = {
+  over: function(event, ui) {
+    $(".legenda-rd").show();
+  },
+  deactivate: function(event, ui) {
+    $(".legenda-rd").hide();
+  },  
   drop: function(event, ui) {
+    $(".legenda-rd").hide();
     $(ui.draggable).remove();
-  }
+  },
+  classes: {
+    "ui-droppable-active": "ui-rd-state-ativado",
+  },
 };
 
 $(".kama-img").draggable(draggableConfig);
 $( ".player" ).sortable(sortableConfig);
-$( "#apagar" ).droppable(droppableConfig);
+$( ".apagar" ).droppable(droppableConfig);
